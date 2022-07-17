f251de1c3c485eaff1b61bfac285b5e3eaf45c83
kim-a-reum <96907807+kim-a-reum@users.noreply.github.com>
2022-07-17 21:33:57 +0900

    7.17

--- src/components/units/contextAPI/contextapi.presenter.tsx
+++ src/components/units/contextAPI/contextapi.presenter.tsx
@@ -3,7 +3,7 @@ import { ExampleContext } from "../../../../pages/contextapi";
 
 const ContextApiUI = () => {
   const { isEdit } = useContext(ExampleContext);
-  return <div>{isEdit ? "수정하기" : "등록하기"}</div>;
+  return <div>{isEdit ? "true일때" : "false일때"}</div>;
 };
 
 export default ContextApiUI;
